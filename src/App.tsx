3c5ed4298c9af560bb9ef90a46be312b95ab62e7
YU WANG <yuwang10@upenn.edu>
2024-07-08 23:20:43 +0900

    app.tsx fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Kanbas from "./Kanbas";
 function App() {
   return (
     <HashRouter>
+      <div className="App">
+      <h1>WEB DEV cs 5610</h1>
+      <h2>Yu Wang 2024 su2</h2>
+      <h2><a href="https://github.com/759257989/kanbas-react-web-app-2024.git">Github</a></h2>
+      </div>
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="Labs" />} />
